e0e15009c04f51e539e5c25fc025f07bc5c27340
Asim <asimkhanwc786@gmail.com>
2026-08-06 00:33:11 +0500

    Giving class name to each button

--- calculator-app/src/App.jsx
+++ calculator-app/src/App.jsx
@@ -9,36 +9,37 @@ function App() {
       </div>
 
       <div className="row">
-        <button>AC</button>
-        <button>DEL</button>
-        <button>/</button>
+        <button className="clear">AC</button>
+        <button className="delete">DEL</button>
+        <button className="operator">/</button>
+
       </div>
 
       <div className="row">
         <button>7</button>
         <button>8</button>
         <button>9</button>
-        <button>*</button>
+        <button className="operator">*</button>
       </div>
 
       <div className="row">
         <button>4</button>
         <button>5</button>
         <button>6</button>
-        <button>-</button>
+        <button className="operator">-</button>
       </div>
 
       <div className="row">
         <button>1</button>
         <button>2</button>
         <button>3</button>
-        <button>+</button>
+        <button className="operator">+</button>
       </div>
 
       <div className="row">
         <button>0</button>
         <button>.</button>
-        <button>=</button>
+        <button className="equal">=</button>
       </div>
 
     </div>
