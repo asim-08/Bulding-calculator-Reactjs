2e1caa1262f2d72b879772169a1eb26a0a09ff34
Asim <asimkhanwc786@gmail.com>
2026-08-05 23:02:06 +0500

    Building the UI of calculator

--- calculator-app/src/App.jsx
+++ calculator-app/src/App.jsx
@@ -1,8 +1,38 @@
 function App() {
   return (
-    <>
-      <h1>Calculator App</h1>
-    </>
+    <div className="calculator">
+      <div className="display">
+        0
+      </div>
+      <div>
+        <button>AC</button>
+        <button>DEL</button>
+        <button>/</button>
+
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>*</button>
+
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+
+        <button>0</button>
+        <button>.</button>
+        <button>=</button>
+
+
+      </div>
+
+    </div>
+
   );
 }
 
